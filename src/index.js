b77378eed91a54a7b54efa21d5853c6dd4d00cfd
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2020-03-19 16:54:24 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+	if (!matrix) {
+		return [];
+	}
+	let arr = [];
+	matrix.forEach((element, index) => {
+		const sorter = index % 2 ? ((a, b) => b - a ) : ((a, b) => a - b);
+		element.sort(sorter);
+		element.forEach(e => {
+			arr.push(e);
+		});
+	});
+	return arr;
 }
